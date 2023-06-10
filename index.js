b322d06d692404fe9be37e4dbdd351a7f65e58e0
Maddy Randle <maddyrandle318@gmail.com>
2023-06-09 23:03:02 -0500

    Adds comments explaining what each array method will do

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ var lotteryNumbers = [4, 67, 32, 99, 44]
 var babyNames = ["Maddy", "Kate", "Payton"]
 var trueOrFalse = [true, false]
 
+// Removes the last element of the babyNames array
 babyNames.pop()
 
+// Checks if the lotteryNumbers array includes the number 44 and returns a boolean
 lotteryNumbers.includes(44)
 
+// Returns a new array containing the second and third element from the lotteryNumbers array
 lotteryNumbers.slice(2, 4)
